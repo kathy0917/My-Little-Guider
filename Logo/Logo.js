d27a211fd5a63583513e856226ee353fe37dd30f
kathy0917 <kathy95917@naver.com>
2021-01-07 02:46:48 +0900

    ArtBoard 프레임 설계

---
+++ Logo/Logo.js
@@ -0,0 +1,10 @@
+import * as url from '../Img/이미지1.svg';
+
+export default function Logo(){
+    const div = document.querySelector(".div");
+
+    const image = document.createElement('img');
+    img.src = url.default;
+    
+    div.appendChild(image);
+}
